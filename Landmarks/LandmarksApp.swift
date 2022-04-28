62a4ed4c83e5abac251cd0e29c89ddc63559b82f
ahnhongjo <ahj0313@ajou.ac.kr>
2022-04-28 15:03:55 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 안홍조 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
